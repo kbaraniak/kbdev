73191f285310b538a7b0d3d62d27a4e089c40d46
kbaraniak <kbraniakdev@gmail.com>
2025-08-10 01:46:08 +0200

    First version of About Me

--- components/Hero/AboutMe.jsx
+++ components/Hero/AboutMe.jsx
@@ -1,8 +1,27 @@
+import Image from "next/image";
+
 export default function AboutMe() {
-    return (
-        <section className="flex justify-center" id="aboutme">
-            <h2 className="text-4xl font-bold">About Me</h2>
-            <div className="w-3xl h-4xl"></div>
-        </section>
-    )
-} 
+  return (
+    <section className="mt-3 flex flex-col justify-center items-center" id="aboutme">
+      <h2 className="text-4xl font-bold">About Me</h2>
+      <div className="mt-3 flex flex-row items-center gap-6 p-6 rounded-lg max-w-4xl w-full">
+        <div className="w-[175px] h-[175px] min-w-[175px] relative rounded-full overflow-hidden bg-gray-500 flex items-center justify-center">
+          {/* Next.js Image with full size and rounded */}
+          <Image
+            src="https://www.gravatar.com/avatar/4a7d1ed414474e4033ac29ccb8653d9b?s=200&d=identicon"
+            alt="Avatar"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-full"
+            priority
+          />
+        </div>
+        <div className="text-xl leading-relaxed">
+          <p>Hi, I'm Kamil.</p>
+          <p>I'm 19 years old. My goal is to help others, and in the future, I plan to revolutionize the world.</p>
+          <p>I especially enjoy creating in frontend development (web apps), but sometimes backend work also brings me joy, mainly using Rust and Python.</p>
+        </div>
+      </div>
+    </section>
+  );
+}
