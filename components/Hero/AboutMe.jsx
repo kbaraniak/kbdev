7d0c2d2b276f7c8070ddedabae9a279baaee90f0
kbaraniak <kbraniakdev@gmail.com>
2025-08-21 03:39:21 +0200

    Enabled responsive for image and text

--- components/Hero/AboutMe.jsx
+++ components/Hero/AboutMe.jsx
@@ -4,8 +4,8 @@ export default function AboutMe() {
   return (
     <section className="mt-3 flex flex-col justify-center items-center" id="aboutme">
       <h2 className="text-4xl font-bold">About Me</h2>
-      <div className="mt-3 flex flex-row items-center gap-6 p-6 rounded-lg max-w-4xl w-full">
-        <div className="w-[175px] h-[175px] min-w-[175px] relative rounded-full overflow-hidden bg-gray-500 flex items-center justify-center">
+      <div className="mt-3 flex flex-col md:flex-row items-center gap-6 p-6 rounded-lg max-w-4xl w-full">
+        <div className="w-[125px] h-[125px] min-w-[125px] md:w-[175px] md:h-[175px] md:min-w-[175px] relative rounded-full overflow-hidden bg-gray-500 flex items-center justify-center">
           {/* Next.js Image with full size and rounded */}
           <Image
             src="https://www.gravatar.com/avatar/4a7d1ed414474e4033ac29ccb8653d9b?s=200&d=identicon"
@@ -16,7 +16,7 @@ export default function AboutMe() {
             priority
           />
         </div>
-        <div className="text-xl leading-relaxed">
+        <div className="max-md:text-center text-xl leading-relaxed">
           <p>Hi, I'm Kamil.</p>
           <p>I'm 19 years old. My goal is to help others, and in the future, I plan to revolutionize the world.</p>
           <p>I especially enjoy creating in frontend development (web apps), but sometimes backend work also brings me joy, mainly using Rust and Python.</p>
