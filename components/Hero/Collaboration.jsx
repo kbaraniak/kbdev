774e2ee44fdb64deb8dffa438b027a2cf7e3b2b7
kbaraniak <kbraniakdev@gmail.com>
2025-08-21 03:27:10 +0200

    Added icons

--- components/Hero/Collaboration.jsx
+++ components/Hero/Collaboration.jsx
@@ -6,18 +6,28 @@ export default function Collaboration() {
             </h2>
             <div className="flex items-center justify-center">
                 <a href="https://github.com/klubuntu">
-                    <div className="mt-3 mx-2 w-60 h-20 bg-red-700 rounded-md text-center flex items-center justify-center hover:opacity-70 transition-opacity">
-                        <p className="text-xl">Klubuntu (Projects)</p>
+                    <div className="mt-3 mx-2 w-61 h-20 bg-red-700 rounded-md text-center flex items-center justify-center hover:opacity-70 transition-opacity">
+                        <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"></path>
+                        </svg>
+                        <p className="mx-1 text-xl">Klubuntu (Projects)</p>
                     </div>
                 </a>
                 <a href="https://github.com/Puri-Devs">
                     <div className="mt-3 mx-2 w-60 h-20 bg-blue-700 rounded-md text-center flex items-center justify-center hover:opacity-70 transition-opacity">
-                        <p className="text-2xl">Puri Devs</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"></path>
+                        </svg>
+                        <p className="mx-1 text-2xl">Puri Devs</p>
                     </div>
                 </a>
                 <a href="https://itvt.xyz">
                     <div className="mt-3 mx-2 w-60 h-20 bg-lime-900 rounded-md text-center flex items-center justify-center hover:opacity-70 transition-opacity">
-                        <p className="text-3xl">iTVT</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 24 24">
+                            <path fill="currentColor" fillRule="evenodd" d="M15.488 1.43a.75.75 0 0 1 .081 1.058L13.202 5.25h2.853c1.367 0 2.47 0 3.337.117c.9.12 1.658.38 2.26.981c.602.602.86 1.36.982 2.26c.116.867.116 1.97.116 3.337v4.11c0 1.367 0 2.47-.116 3.337c-.122.9-.38 1.658-.982 2.26s-1.36.86-2.26.982c-.867.116-1.97.116-3.337.116h-8.11c-1.367 0-2.47 0-3.337-.116c-.9-.122-1.658-.38-2.26-.982s-.86-1.36-.981-2.26c-.117-.867-.117-1.97-.117-3.337v-4.11c0-1.367 0-2.47.117-3.337c.12-.9.38-1.658.981-2.26c.602-.602 1.36-.86 2.26-.981c.867-.117 1.97-.117 3.337-.117h2.853L8.43 2.488a.75.75 0 1 1 1.138-.976L12 4.348l2.43-2.836a.75.75 0 0 1 1.058-.081m1.262 5.32v14.5c1.046-.004 1.826-.02 2.442-.103c.734-.099 1.122-.28 1.399-.556c.277-.277.457-.665.556-1.4c.101-.755.103-1.756.103-3.191v-4c0-1.435-.002-2.437-.103-3.192c-.099-.734-.28-1.122-.556-1.399c-.277-.277-.665-.457-1.4-.556c-.615-.083-1.395-.099-2.441-.102m-1.5 14.5V6.75H8c-1.435 0-2.437.002-3.192.103c-.734.099-1.122.28-1.399.556c-.277.277-.457.665-.556 1.4c-.101.754-.103 1.756-.103 3.191v4c0 1.435.002 2.436.103 3.192c.099.734.28 1.122.556 1.399c.277.277.665.457 1.4.556c.754.101 1.756.103 3.191.103z" clipRule="evenodd" strokeWidth={0.5} stroke="currentColor"></path>
+                            <path fill="currentColor" d="M20 16a1 1 0 1 0-2 0a1 1 0 0 0 2 0m0-4a1 1 0 1 0-2 0a1 1 0 0 0 2 0" strokeWidth={0.5} stroke="currentColor"></path>
+                        </svg>
+                        <p className="mx-2 text-3xl">iTVT</p>
                     </div>
                 </a> 
             </div>
