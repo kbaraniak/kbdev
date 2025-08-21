a38cb113fa74bae26f3c55adca8bfbd81d4f3565
kbaraniak <kbraniakdev@gmail.com>
2025-08-21 02:32:29 +0200

    Updated projects section

--- components/Hero/Projects.jsx
+++ components/Hero/Projects.jsx
@@ -10,9 +10,9 @@ export default function Projects() {
 
         {/* Right column: projects grid */}
             <div className="mt-3 grid grid-flow-col grid-rows-2 gap-10 justify-center">
-                <Box id="1" website_url="" source_url="" />
-                <Box id="2" website_url="" source_url="" />
-                <Box id="3" website_url="" source_url="" />
+                <Box id="1" website_url="" source_url="" stack={["react", "typescript"]}/>
+                <Box id="2" website_url="" source_url="" stack={["css"]}/>
+                <Box id="3" website_url="" source_url="" stack={["python"]}/>
                 <Box id="4" website_url="" source_url="" />
                 <Box id="5" website_url="" source_url="" />
             </div>
